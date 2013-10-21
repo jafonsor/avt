bd8c770d69b50bbd5f72963fa1674bb721d8044f
João Rodrigues <jrodriguesnum1@hotmail.com>
2013-10-21 20:16:07 +0100

    works on ubuntu if the libraries are installed

--- projects/assignment1/src/assignment_1.cpp
+++ projects/assignment1/src/assignment_1.cpp
@@ -108,8 +108,8 @@ void setupGLUT(int argc, char* argv[])
 {
 	glutInit(&argc, argv);
 	
-	glutInitContextVersion(3, 3);
-	//glutInitContextFlags(GLUT_FORWARD_COMPATIBLE);
+	glutInitContextVersion(4, 2);
+	glutInitContextFlags(GLUT_FORWARD_COMPATIBLE);
 	glutInitContextProfile(GLUT_CORE_PROFILE);
 
 	glutSetOption(GLUT_ACTION_ON_WINDOW_CLOSE,GLUT_ACTION_GLUTMAINLOOP_RETURNS);
